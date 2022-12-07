00232dc5bd1512c4d7c292d7bb5460d794cc4194
Clay Loftus <clay.loftus@flatsorspikes.com>
2022-12-06 17:28:35 -0700

    Updated Homepage UI

--- sleep-ui/src/pages/Home.js
+++ sleep-ui/src/pages/Home.js
@@ -4,7 +4,10 @@ import Button from '../ui-components/Button';
 function Home() {
 
     return (
-        <div>
+        <div className='container'>
+            <div className='title'>
+                <h1>Sleep Diary</h1>
+            </div>
             <Button buttonText={"Add log"} buttonPath={'/new-entry/date'}/>
         </div>
     )
