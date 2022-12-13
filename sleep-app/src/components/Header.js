ad6783299b010f6963c8bfb8ed8ec6f7f5ede6c0
Clay Loftus <clay.loftus@flatsorspikes.com>
2022-12-13 15:51:55 -0700

    Pass state and pah props to button

--- sleep-app/src/components/Header.js
+++ sleep-app/src/components/Header.js
@@ -4,13 +4,17 @@ import Button from "./Button";
 
 function Header ({tabs, button}) {
 
-    const headerTabs = tabs.map((tab, i) => <div className="tab" key={i}>{tab.name}</div>);
+    const headerTabs = tabs.map((tab, i) =>
+        <div className="tab" key={i} path={tab.path} onClick={() => alert(`Opens ${tab.path}`)}>
+            {tab.name}
+        </div>
+    )
     
     return (
         <div className="header">
             <h1>sleepTracker</h1>
             <div className="tab-set">{headerTabs}</div>
-            <Button text={button.text} />
+            <Button text={button.text} path={button.path} active={button.active}/>
         </div>
     )
 }
