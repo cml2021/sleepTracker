f0ec10570a93da9b97417efd3a2b9ae1aaca9002
Clay Loftus <clay.loftus@flatsorspikes.com>
2022-12-13 15:51:15 -0700

    Add click handler and disabled state

--- sleep-app/src/components/Button.js
+++ sleep-app/src/components/Button.js
@@ -1,9 +1,19 @@
 import React from "react";
 import "../styles/Button.css"
 
-function Button ({text, action}) {
+function Button ({text, path, active}) {
+
+    const buttonClick = (event) => {
+        event.preventDefault();
+        if (active === true) {
+            alert(`Opens ${path}`)
+        }
+    }
+
+    const className = active === true ? "button" : "button disabled";
+
     return (
-        <div class="button">
+        <div className={className} onClick={buttonClick}>
             {text}
         </div>
     )
